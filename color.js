0699d2e03fb335114fc8c95209b8b24aa7a42a91
pirate1234 <65395286+pirate1234@users.noreply.github.com>
2022-01-02 14:16:29 +0900

    Add files via upload

--- color.js
+++ color.js
@@ -1,45 +1,53 @@
-        // function hsetcolor(color){
-        //     var alist = document.querySelectorAll('a');
-        //         var i = 0;
-        //         while(i < alist.length){
-        //             alist[i].style.color = color;
-        //             i = i + 1;
-        //         }
+// function hsetcolor(color){
+//     var alist = document.querySelectorAll('a');
+//         var i = 0;
+//         while(i < alist.length){
+//             alist[i].style.color = color;
+//             i = i + 1;
+//         }
+// }
+// function bodysetcolor(color){
+//     document.querySelector('body').style.color = color;
+// }
+// function bodysetbackground(color){
+//     document.querySelector('body').style.backgroundColor = color;
+// }
+var body = {
+    setcolor: function (color) {
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setbackground: function (color) {
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
+    },
+    setborder: function (color) {
+        $('.myborder').css('border-color', color);
+    }
+}
+var h = {
+    setcolor: function (color) {
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i = i + 1;
         // }
-        // function bodysetcolor(color){
-        //     document.querySelector('body').style.color = color;
-        // }
-        // function bodysetbackground(color){
-        //     document.querySelector('body').style.backgroundColor = color;
-        // }
-        var body = {
-            setcolor: function (color) {
-                document.querySelector('body').style.color = color;
-            },
-            setbackground: function (color) {
-                document.querySelector('body').style.backgroundColor = color;
-            },
-        }
-        var h = {
-            setcolor: function (color) {
-                var alist = document.querySelectorAll('a');
-                var i = 0;
-                while (i < alist.length) {
-                    alist[i].style.color = color;
-                    i = i + 1;
-                }
-            }
-        }
-        function daynight(hot) {
-            if (hot.value === 'night') {
-                body.setbackground('black');
-                body.setcolor('white');
-                hot.value = 'day';
-                h.setcolor('blue');
-            } else {
-                body.setbackground('white');
-                body.setcolor('black');
-                hot.value = 'night';
-                h.setcolor('red');
-            }
-        }
+        $('a').css('color', color);
+    }
+}
+function daynight(hot) {
+    if (hot.value === 'night') {
+        body.setbackground('black');
+        body.setcolor('white');
+        hot.value = 'day';
+        h.setcolor('blue');
+        body.setborder('white')
+    } else {
+        body.setbackground('white');
+        body.setcolor('black');
+        hot.value = 'night';
+        h.setcolor('red');
+        body.setborder('black');
+    }
+}
